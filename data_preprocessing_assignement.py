aad9f029e014d4b11a1c2c37627b96b2ce524e88
LenouarMiloud <lenouarmiloud@gmail.com>
2020-01-04 22:32:08 +0100

    solve the Problem

--- data_preprocessing_assignement.py
+++ data_preprocessing_assignement.py
@@ -16,14 +16,26 @@ Happy coding."""
 # Step 0: import the necessary libraries: pandas, matplotlib.pyplot, and numpy.
     import numpy as np
     import pandas as pd 
-    import matplotlib.pyplot as plt 
+    import matplotlib.pyplot as plt
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+
 # Step 1: load your dataset using pandas
     data = pd.read_csv('Social_Network_Ads.csv')
+    x = data.drop(['User ID', 'Purchased'], axis = 1)
+	y = data['Purchased']
+
 # Step 2: Handle Missing data if they exist.
-    data.fillna(data.mean(), inplace=True)
+    X.isnull().sum()
+
 # Step 3: Encode the categorical variables.
     df = pd.get_dummies(data,prefix="",columns=['Gender'])
+    encoder = LabelEncoder()
+    x['Gender'] = encoder.fit_transform(X['Gender'])
 # Step 4: Do Feature Scaling if necessary.
-    
+    scaler = StandardScaler()
+	x = scaler.fit_transform(X)
 # Final Step: Train/Test Splitting.
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 19)
     
